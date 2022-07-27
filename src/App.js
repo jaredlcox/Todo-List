8bcfacae649d4769eb4e835978f31fe5d8ce5439
Jared Cox <96136301+jaredlcox@users.noreply.github.com>
2022-07-27 12:44:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import './App.css';
+// creating this todo using class-based componenets
+// managing the state using class-based components
+// managing state using functional components is a more modern practice by using React Hooks
+// There's a class-based method called componentDidCatch that is not available in Hooks and actually requires setting state using class methods
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    // constructor(props) { super(props) is used for passing data from one component to another
     this.state = {
       newItem: "",
       list: []
     };
+    // there can be as many properties in the this.state object as you like
+    // every time the state of an object changes, React re-renders the component to the browser
+    // right now the list is just an empty array with an empty string located in the array
+    // when we update the state by addng newItem to the todo list, this.state will re-render with an updated list of text
+
   }
 
   updateInput(key, value) {
-    // update react state
-    this.setState({ [key]: value });
+    // this.setState() is used to change the value of the state object
+    // this is what is being put into the "Add an Item" input box
+    // without this we wouldn't be able to add text to the input field
+
+    this.setState({ [key]: value})
   }
 
   addItem() {
-    // create a new item with unique id
+    // create a new item with unique id although Math.random could in theory generate the same random number which would not allow for a unique id to be generated
     const newItem = {
       id: 1 + Math.random(),
-      value: this.state.newItem.slice()
+      value: this.state.newItem
  
     };
 
@@ -38,6 +49,7 @@ class App extends React.Component {
 
   deleteItem(id) {
     // copy current list of items
+    // look at the addItem function above and notice that these are the same. Used to copy the current list of items so that's why it's used for both delete and add
     const list = [...this.state.list];
     // filter out the item being deleted
     const updatedList = list.filter(item => item.id !== id);
@@ -61,7 +73,7 @@ class App extends React.Component {
           }}
         >
           Add an Item...
-          <br />
+          <br/>
           <input
             type="text"
             placeholder="Type item here"
@@ -69,6 +81,7 @@ class App extends React.Component {
             onChange={e => this.updateInput("newItem", e.target.value)}
           />
           <button
+          // this button is for the user to input text on the input
             className="add-btn btn-floating"
             onClick={() => this.addItem()}
             disabled={!this.state.newItem.length}
@@ -77,9 +90,11 @@ class App extends React.Component {
           </button>
           <br /> <br />
           <ul>
+            {/* this will take the current state of list and map over that */}
             {this.state.list.map(item => {
               return (
                 <li key={item.id}>
+                  {/* need to use key every item.id should be unique */}
                   {item.value}
                   <button className="btn btn-floating" onClick={() => this.deleteItem(item.id)}>
                     <i class="material-icons">x</i>
